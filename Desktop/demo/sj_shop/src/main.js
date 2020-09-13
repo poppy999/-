3f0128c40a8740d128b042d0b6baf5f9e2eac37f
羊羊毛卷卷 <zxtjuan@163.com>
2020-09-13 16:42:49 +0800

    add files

--- Desktop/demo/sj_shop/src/main.js
+++ Desktop/demo/sj_shop/src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
-import './styles.scss'
-
-Vue.use(ElementUI, { locale })
+import router from './router'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
